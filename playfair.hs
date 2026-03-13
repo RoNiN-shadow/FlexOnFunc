873f2d4d13cf906a75ec61c8e8f897e87f06e908
Mark <marktishuk@gmail.com>
2026-03-13 18:05:18 +0100

    add playfair funcs and cli playfair

--- playfair.hs
+++ playfair.hs
@@ -1,10 +1,12 @@
-import Data.List (elemIndex)
+import Data.List (elemIndex, nub)
 import Data.Maybe (fromJust)
+import Data.Char (toUpper)
+import System.Environment (getArgs)
 
 countChar :: Char -> String -> Int
 countChar c (x:rest)
   |  c == x    = 1 + countChar c rest
-  | otherwise  = 0 + countChar x xs
+  | otherwise  = 0 + countChar x rest
 
 countChar _ [] = 0
 
@@ -64,7 +66,7 @@ takeEverySecond (a:b:rest) = a : takeEverySecond rest
 takeEverySecond [x] = [x]
 takeEverySecond [] = []
 
-coutWeak :: [Int] -> Int
+countWeak :: [Int] -> Int
 countWeak (x:xs)
   | x < 20    = 1 + countWeak xs
   | otherwise = 0 + countWeak xs
@@ -76,8 +78,34 @@ getFirstWord (x:xs)
   | otherwise = x : getFirstWord xs
 getFirstWord [] = []
 
-encryptPair :: String -> (Char, Char) -> (Char, Char)
-encryptPair grid (a, b) =
+alphabet :: String
+alphabet = filter (/= 'W')['A'..'Z']
+
+formatKey :: String -> String
+formatKey (x:xs)
+  | x == 'W'  = 'V': formatKey xs 
+  | otherwise = toUpper x : formatKey xs
+formatKey [] = []
+
+buildGrid :: String -> String
+buildGrid key = nub(formatKey key ++ alphabet)
+
+prepareText :: String -> String 
+prepareText (a:b:rest)
+  | a == b    = a : 'X' : prepareText(b:rest)
+  | otherwise = a : b : prepareText rest
+prepareText [a] = [a, 'X']
+prepareText [] = []
+
+toPairs :: String -> [(Char, Char)]
+toPairs (a:b:rest) = (a, b) : toPairs rest
+
+toPairs _ = []
+
+data Mode = Encrypt | Decrypt
+
+processPair :: Mode -> String -> (Char, Char) -> (Char, Char)
+processPair mode grid (a, b) =
   let 
       indexA = fromJust (elemIndex a grid)
       indexB = fromJust (elemIndex b grid)
@@ -91,18 +119,57 @@ encryptPair grid (a, b) =
 
   where
     
+    shift = case mode of
+            Encrypt -> 1
+            Decrypt -> -1
     applyRules rA cA rB cB
       | rA == rB =
-          let newColA = (cA + 1) `mod` 5
-              newColB = (cB + 1) `mod` 5
+          let newColA = (cA + shift) `mod` 5
+              newColB = (cB + shift) `mod` 5
           in (getCharByCoords rA newColA, getCharByCoords rB newColB)
       | cA == cB =
-          let newRowA = (rA + 1 ) `mod` 5
-              newRowB = (rB + 1 ) `mod` 5
+          let newRowA = (rA + shift ) `mod` 5
+              newRowB = (rB + shift ) `mod` 5
           in (getCharByCoords newRowA cA, getCharByCoords newRowB cB)
 
       | otherwise = (getCharByCoords rA cB, getCharByCoords rB cA)
     
     getCharByCoords r c = grid !! (r * 5 + c)
+
+pairsToString :: [(Char, Char)] -> String
+pairsToString [] = []
+pairsToString ((a, b):rest) = a : b : pairsToString rest
+
+cryptMessage :: Mode -> String -> String -> String
+cryptMessage mode key message =
+  let 
+      grid = buildGrid key
+
+      cleanText = prepareText (formatKey message)
+
+      pairs = toPairs cleanText
+
+      processPairs = map (processPair mode grid) pairs
+
+      result = pairsToString processPairs
+  in result
+
+
+main :: IO ()
+main = do
+    args <- getArgs
+
+    if length args < 3
+      then putStrLn "Using: playfair <encrypt|decrypt> <KEY> <MESSAGE>"
+    else do
+      let modeStr = args !! 0
+      let key = args !! 1
+      let msg = args !! 2
+
+      let mode = if modeStr == "encrypt" then Encrypt else Decrypt
+
+      let result = cryptMessage mode key msg
+
+      putStrLn result
 
 
