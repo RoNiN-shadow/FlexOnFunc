d0482e281de79e171955b8322de08cee4a70d4de
Mark <marktishuk@gmail.com>
2026-03-22 12:26:46 +0100

    add data type with instance for Show

--- learn-haskell/exersice-datatypes/exercise.hs
+++ learn-haskell/exersice-datatypes/exercise.hs
@@ -15,3 +15,20 @@ isPalindrome s = clean s == reverse (clean s)
 
 fibs :: [Int]
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+
+data Shape = Circle Double
+            | Reactangle Double Double
+            | Triangle Double Double Double
+
+area :: Shape -> Double
+area (Circle r) = 3.14 * r *r 
+area (Reactangle a b) = a * b
+area (Triangle a b c) = sqrt (s *(s-a)*(s-b)*(s-c))
+      where
+        s = (a+b+c) / 2
+
+instance Show Shape where
+      show (Circle r) = "Circle with radius " ++ show r
+      show(Reactangle a b) = "Reactangle " ++ show a ++ "x" ++ show b
+      show (Triangle a b c) = "Triangle " ++ show a ++ " " ++ show b ++ " " ++ show c
