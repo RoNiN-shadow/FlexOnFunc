1943ef3ef2a7722a359f01f37a011386c61cbdda
Mark <marktishuk@gmail.com>
2026-03-22 11:30:38 +0100

    implement pattern matching

--- learn-haskell/baby.hs
+++ learn-haskell/baby.hs
@@ -8,3 +8,16 @@ doubleSmallNumber x = if x <= 100
                     else x
 
 doubleSmallNumber' x = (if x <= 100 then x*2 else x) + 1
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1+x2, y1+y2)
+
+head' :: [a] -> a
+head' [] = error "Can't tell head on an empty list, damn!"
+head'(x:_) = x 
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has only two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "This list is long, here are first two elements: " ++ show x ++ " and " ++ show y
