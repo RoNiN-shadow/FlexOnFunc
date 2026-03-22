c48de0d0031f9f77b9aadc3b4849bedcd41cb466
Mark <marktishuk@gmail.com>
2026-03-22 11:35:44 +0100

    add pattern matching on recursion

--- learn-haskell/baby.hs
+++ learn-haskell/baby.hs
@@ -21,3 +21,7 @@ tell [] = "The list is empty"
 tell [x] = "The list has one element: " ++ show x
 tell [x,y] = "The list has only two elements: " ++ show x ++ " and " ++ show y
 tell (x:y:_) = "This list is long, here are first two elements: " ++ show x ++ " and " ++ show y
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
