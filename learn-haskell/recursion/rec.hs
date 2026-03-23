eacfc688ad12a52a302e3f64ca57d112dd565910
Mark <marktishuk@gmail.com>
2026-03-23 11:35:58 +0100

    add replicate recursivly

--- learn-haskell/recursion/rec.hs
+++ learn-haskell/recursion/rec.hs
@@ -6,3 +6,7 @@ maximum' [] = error "Couldn't maximum of empty list"
 maximum' [x] = x
 maximum' (x:xs) = max x (maximum' xs)
   
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+  | n <= 0    = []
+  | otherwise = x : replicate' (n-1) x
