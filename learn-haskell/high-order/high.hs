0a853adca6f5644917aae60a0be1a3fb7cc6eca7
Mark <marktishuk@gmail.com>
2026-03-23 12:49:53 +0100

    implement filtering on infinite lists

--- learn-haskell/high-order/high.hs
+++ learn-haskell/high-order/high.hs
@@ -16,3 +16,18 @@ quicksort (x:xs) =
       smaller = quicksort ( filter (<=x) xs)
       bigger  = quicksort ( filter (>x) xs)
   in smaller ++ [x] ++ bigger
+
+largestDivision :: (Integral a) => a
+largestDivision = head (filter p [100000, 99999..])
+    where p x = x `mod` 3829 == 0
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+  | even n  = n : chain (n `div` 2) 
+  | odd n   = n : chain (n*3 +1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+    where
+      isLong xs = length xs > 15
