c9db329271ba65219055f442fa1c4e8f6a5a1091
Mark <marktishuk@gmail.com>
2026-03-23 12:23:59 +0100

    implement quicksort with filter

--- learn-haskell/high-order/high.hs
+++ learn-haskell/high-order/high.hs
@@ -8,3 +8,11 @@ zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 flip' :: (a -> b -> c) -> (b -> a -> c)
 flip' f y x = f x y
 
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+  let
+      smaller = quicksort ( filter (<=x) xs)
+      bigger  = quicksort ( filter (>x) xs)
+  in smaller ++ [x] ++ bigger
