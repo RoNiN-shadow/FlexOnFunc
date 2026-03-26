a814e61415793ee8d19115b6e20ab14ca848cdb3
Mark <marktishuk@gmail.com>
2026-03-26 16:03:26 +0100

    add cabal and random module

---
+++ learn-haskell/random/random.hs
@@ -0,0 +1,6 @@
+import System.Random (randomR, StdGen)
+
+type Point = (Int, Int)
+data BoardInfo = BoardInfo {height :: Int, width :: Int} deriving (Show, Eq)
+
+makeRandomPoint :: BoardInfo -> StdGen -> (Point, StdGen)
