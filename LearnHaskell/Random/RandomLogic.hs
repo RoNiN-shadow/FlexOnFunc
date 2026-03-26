cc247300e3fb4fe914fabd9212ab91fc9f9070be
Mark <marktishuk@gmail.com>
2026-03-26 16:17:31 +0100

    implement Random point logic

--- LearnHaskell/Random/RandomLogic.hs
+++ LearnHaskell/Random/RandomLogic.hs
@@ -1,8 +1,14 @@
 module LearnHaskell.Random.RandomLogic where
 
-import System.Random (randomR, StdGen)
+import System.Random (uniformR, StdGen)
 
 type Point = (Int, Int)
 data BoardInfo = BoardInfo {height :: Int, width :: Int} deriving (Show, Eq)
 
 makeRandomPoint :: BoardInfo -> StdGen -> (Point, StdGen)
+makeRandomPoint BoardInfo{width = w, height = h} g =
+    let
+        (x, g1) = uniformR (0, w - 1) g
+        (y, g2) = uniformR (0, h - 1) g1
+        p = (x, y)
+    in (p, g2)
