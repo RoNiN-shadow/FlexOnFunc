0b52d7212c56e434abe1245a6ecbc0808d596fe0
Mark <marktishuk@gmail.com>
2026-03-27 16:18:00 +0100

    test the function inSeq

--- Main.hs
+++ Main.hs
@@ -1,4 +1,12 @@
 module Main where
+import LearnHaskell.Random.RandomLogic (inSeq)
+import Data.Sequence (fromList)
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main =
+    let
+      seqq = fromList [(0,1), (2,4), (4,5)]
+      p   = (2,4)
+      p1  = (5,4)
+    in putStrLn $ show(inSeq p seqq) ++ " " ++ show(inSeq p1 seqq)
+    
