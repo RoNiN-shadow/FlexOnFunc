98b6788897fe274da078dfa6db93cc37978bcda6
Mark <marktishuk@gmail.com>
2026-03-27 16:17:29 +0100

    add InSeq to check if point in the seq

--- LearnHaskell/Random/RandomLogic.hs
+++ LearnHaskell/Random/RandomLogic.hs
@@ -1,6 +1,7 @@
 module LearnHaskell.Random.RandomLogic where
 
 import System.Random (uniformR, StdGen)
+import Data.Sequence (Seq(..))
 
 type Point = (Int, Int)
 data BoardInfo = BoardInfo {height :: Int, width :: Int} deriving (Show, Eq)
@@ -12,3 +13,9 @@ makeRandomPoint BoardInfo{width = w, height = h} g =
         (y, g2) = uniformR (0, h - 1) g1
         p = (x, y)
     in (p, g2)
+
+inSeq :: Point -> Seq Point -> Bool
+inSeq _ Empty = False 
+inSeq p (x:<|xs)
+    | p == x    = True
+    | otherwise = inSeq p xs
